1011a2091a77293be23006392f52725d3a7bb18d
Wesley Reisz <wes@wesleyreisz.com>
2014-10-13 12:04:36 -0400

    dispatch to camera to get picture

--- week5/projects/Samples/app/src/main/java/com/wesleyreisz/samples/CameraActivity.java
+++ week5/projects/Samples/app/src/main/java/com/wesleyreisz/samples/CameraActivity.java
@@ -1,17 +1,36 @@
 package com.wesleyreisz.samples;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class CameraActivity extends Activity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        Button button = (Button)findViewById(R.id.btnCamera);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
 
