7b836cac67c7384bd71c51cee58f9baea3d20911
Wesley Reisz <wes@wesleyreisz.com>
2014-11-02 11:21:12 -0500

    Example showing handling multiple threads and using user defined methods

--- week12/app/src/main/java/com/wesleyreisz/counters/MainActivity.java
+++ week12/app/src/main/java/com/wesleyreisz/counters/MainActivity.java
@@ -13,22 +13,22 @@ import android.widget.TextView;
 import android.os.Handler;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+    private int threadsCount=0;
     private TextView mTxtView;
-
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            mTxtView.setText((String)msg.obj);
-        }
-    };
+    private TextView mTextMessage;
+    private CounterHandler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTxtView = (TextView)findViewById(R.id.txtView);
+        mTextMessage = (TextView)findViewById(R.id.txtMessage);
+
         Button btnStart = (Button)findViewById(R.id.btnStart);
         btnStart.setOnClickListener(this);
+
+        mHandler = new CounterHandler();
     }
 
     @Override
@@ -55,32 +55,57 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-       new Thread(new MyCounter()).start();
+        //mTextMessage.setText("Started...");
+        new Thread(new MyCounter(++threadsCount)).start();
+        //mTextMessage.setText("Finished...");
+    }
+
+    class CounterHandler extends Handler{
+        public void setCounter(final String counterText){
+            runOnUiThread( new Runnable() {
+                public void run() {
+                    mTxtView.setText(counterText);
+                }
+            });
+        }
+        public void setMessage(final String counterText){
+            runOnUiThread( new Runnable() {
+                public void run() {
+                    mTextMessage.setText(counterText);
+                }
+            });
+        }
     }
 
     class MyCounter implements Runnable {
+        int localCount=0;
+        public MyCounter(int threadNumber){
+            localCount=threadNumber;
+        }
         @Override
         public void run() {
             int seconds = 0;
+            mHandler.setMessage("Started: " + localCount + "!");
             while(seconds<20){
                 try {
                     seconds++;
                     Thread.sleep(1000);
                     String stringMessage = "Counter: " + seconds;
-                    Message msg = Message.obtain();
-                    msg.obj = stringMessage;
-                    handler.sendMessage(msg);
+                    mHandler.setCounter(stringMessage);
                     Log.d("Counter", stringMessage);
+
+                    if(seconds%10==0){
+                        mHandler.setMessage(
+                            "Multiple of 10: " + seconds + " on thread #: " +localCount
+                        );
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+            mHandler.setMessage("Finished: " + localCount + "!");
         }
     }
 
-    class CounterHandler extends Handler{
-        public void setCounter(String counterText){
-            mTxtView.setText(counterText);
-        }
-    }
+
 }
