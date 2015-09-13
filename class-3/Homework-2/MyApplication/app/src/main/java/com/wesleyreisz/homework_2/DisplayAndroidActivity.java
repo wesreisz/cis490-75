aba366e76fab8b9025ea693deea57f43b2b093c4
Wesley Reisz <wes@wesleyreisz.com>
2015-09-13 13:00:01 -0400

    Homework 2 Complete
    This homework is based on the first android application homework
    from https://developer.android.com/training/basics/firstapp/index.html
    It expands only slightly on it by asking the student to import an
    image into the drawable folder and reference. The last part of the
    homework the student it challenged to use internet resources to
    figure out how to add the image.

--- class-3/Homework-2/MyApplication/app/src/main/java/com/wesleyreisz/homework_2/DisplayAndroidActivity.java
+++ class-3/Homework-2/MyApplication/app/src/main/java/com/wesleyreisz/homework_2/DisplayAndroidActivity.java
@@ -1,27 +1,22 @@
 package com.wesleyreisz.homework_2;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
-public class MyActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "com.wesleyreisz.homework_2.extra_message";
+public class DisplayAndroidActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_display_android);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
+        getMenuInflater().inflate(R.menu.menu_display_android, menu);
         return true;
     }
 
@@ -38,20 +33,5 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText)findViewById(R.id.edtMessage);
-        String message =editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    /** Called when the user clicks the Send button */
-    public void launchWhatsUp(View view) {
-        Intent intent = new Intent(this, DisplayAndroidActivity.class);
-        startActivity(intent);
     }
 }
