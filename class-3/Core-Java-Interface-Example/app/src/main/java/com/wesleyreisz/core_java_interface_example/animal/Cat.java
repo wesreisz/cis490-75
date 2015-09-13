9c88b9e7faa22c2b40a2c1b1f43d11a78904863a
Wesley Reisz <wes@wesleyreisz.com>
2015-09-13 15:00:35 -0400

    added sounds

--- class-3/Core-Java-Interface-Example/app/src/main/java/com/wesleyreisz/core_java_interface_example/animal/Cat.java
+++ class-3/Core-Java-Interface-Example/app/src/main/java/com/wesleyreisz/core_java_interface_example/animal/Cat.java
@@ -4,4 +4,8 @@ package com.wesleyreisz.core_java_interface_example.animal;
  * Created by wesleyreisz on 9/13/15.
  */
 public class Cat extends Animal {
+    @Override
+    public String speak() {
+        return "Meow";
+    }
 }
