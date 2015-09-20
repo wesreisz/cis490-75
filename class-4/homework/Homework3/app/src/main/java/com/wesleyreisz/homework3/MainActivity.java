f8d4027ec4a6a18965a7149f68da58e9f7b7009a
Wesley Reisz <wes@wesleyreisz.com>
2015-09-20 16:37:31 -0400

    Added EditView Text
    Add some text that the user enters and then it is displayed
    in the Android applicaiton.

--- class-4/homework/Homework3/app/src/main/java/com/wesleyreisz/homework3/MainActivity.java
+++ class-4/homework/Homework3/app/src/main/java/com/wesleyreisz/homework3/MainActivity.java
@@ -5,13 +5,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView mainTextView;
     Button mainButton;
-    
+    EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList<String> mNameList = new ArrayList<String>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // and listen for it here
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
+
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        // 4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        // Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+
+// Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
+
+
     }
 
     @Override
@@ -37,7 +62,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        // Test the Button
-        mainTextView.setText("Button pressed!");
+        // Take what was typed into the EditText
+        // and use in TextView
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
+        // Also add that value to the list shown in the ListView
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
     }
 }
