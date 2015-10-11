84a2373ffe8680e00f564b4c10465c21b4371642
Wesley Reisz <wes@wesleyreisz.com>
2015-10-11 17:20:56 -0400

    Renamed file

--- class-6/Android-Fragments-Designtime/AndroidVersions/app/src/main/java/com/wesleyreisz/androidversions/FragmentContent.java
+++ class-6/Android-Fragments-Designtime/AndroidVersions/app/src/main/java/com/wesleyreisz/androidversions/FragmentContent.java
@@ -13,6 +13,6 @@ public class FragmentContent extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_fragment_content, container, false);
+        return inflater.inflate(R.layout.fragment_content, container, false);
     }
 }
