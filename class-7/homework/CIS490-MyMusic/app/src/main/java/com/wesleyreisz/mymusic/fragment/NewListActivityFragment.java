086e939de1dc7eb85afddf30763a118887f9926d
Wesley Reisz <wes@wesleyreisz.com>
2015-10-18 17:35:10 -0400

    Added onItemClickListener

--- class-7/homework/CIS490-MyMusic/app/src/main/java/com/wesleyreisz/mymusic/fragment/NewListActivityFragment.java
+++ class-7/homework/CIS490-MyMusic/app/src/main/java/com/wesleyreisz/mymusic/fragment/NewListActivityFragment.java
@@ -5,14 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.wesleyreisz.mymusic.R;
 import com.wesleyreisz.mymusic.model.Song;
 import com.wesleyreisz.mymusic.service.MockMusicService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -37,6 +37,14 @@ public class NewListActivityFragment extends Fragment {
                 songs);
 
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(getActivity(),"Position: " + position, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
 
         return view;
     }
