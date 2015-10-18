79008211b13ae27461bb990739f002059316136a
Wesley Reisz <wes@wesleyreisz.com>
2015-10-18 17:48:42 -0400

    Change declarative fragment definition to programmatic

--- class-7/homework/CIS490-MyMusic/app/src/main/java/com/wesleyreisz/mymusic/NewListActivity.java
+++ class-7/homework/CIS490-MyMusic/app/src/main/java/com/wesleyreisz/mymusic/NewListActivity.java
@@ -1,7 +1,13 @@
 package com.wesleyreisz.mymusic;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Activity;
+
+import com.wesleyreisz.mymusic.fragment.ListFragment;
+import com.wesleyreisz.mymusic.fragment.MyListFragment;
+import com.wesleyreisz.mymusic.fragment.NewListActivityFragment;
 
 public class NewListActivity extends Activity {
 
@@ -9,6 +15,12 @@ public class NewListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_list);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        NewListActivityFragment listFragment = new NewListActivityFragment();
+        fragmentTransaction.add(R.id.fragmentContainer,listFragment);
+        fragmentTransaction.commit();
     }
 
 }
