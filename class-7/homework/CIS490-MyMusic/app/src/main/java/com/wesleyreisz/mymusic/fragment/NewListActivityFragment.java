dc935c1e9938cb925b021ed20a5a402876257440
Wesley Reisz <wes@wesleyreisz.com>
2015-10-18 16:58:29 -0400

    Basic ArrayAdapter

--- class-7/homework/CIS490-MyMusic/app/src/main/java/com/wesleyreisz/mymusic/fragment/NewListActivityFragment.java
+++ class-7/homework/CIS490-MyMusic/app/src/main/java/com/wesleyreisz/mymusic/fragment/NewListActivityFragment.java
@@ -5,8 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.wesleyreisz.mymusic.R;
+import com.wesleyreisz.mymusic.model.Song;
+import com.wesleyreisz.mymusic.service.MockMusicService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,6 +26,22 @@ public class NewListActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_new_list, container, false);
+        View view =  inflater.inflate(R.layout.fragment_new_list, container, false);
+
+        ListView listView = (ListView) view.findViewById(R.id.listViewSongs);
+        List<Song> songs = new MockMusicService().findAll();
+        ArrayList songNames = new ArrayList();
+        for(Song song : songs){
+            songNames.add(song.getSongTitle());
+        }
+
+        // Create an ArrayAdapter for the ListView
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(),
+                android.R.layout.simple_list_item_1,
+                songNames);
+
+        listView.setAdapter(arrayAdapter);
+
+        return view;
     }
 }
