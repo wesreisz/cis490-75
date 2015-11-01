63551767d58772498e458562b08d54708678274f
Wesley Reisz <wesleyreisz@Wesleys-MacBook-Pro-2.local>
2015-11-01 17:28:03 -0500

    Sample Applicaton Working with Parse

--- class-9/Parse-Starter-Project-1.10.3/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ class-9/Parse-Starter-Project-1.10.3/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -9,21 +9,25 @@
 package com.parse.starter;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
 
-    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+      ParseAnalytics.trackAppOpenedInBackground(getIntent());
+      ParseObject testObject = new ParseObject("TestObject");
+      testObject.put("foo", "bar");
+      testObject.saveInBackground();
   }
 
   @Override
