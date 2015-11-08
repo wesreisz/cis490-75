ca733ae555b02cb5877e6d87d681bc66b72e46fe
Wesley Reisz <wes@wesleyreisz.com>
2015-11-08 11:50:43 -0500

    Initial Project Configured with Parse

--- class-10/homework-part-a/TodoList/app/src/main/java/com/example/wesleyreisz/todolist/App.java
+++ class-10/homework-part-a/TodoList/app/src/main/java/com/example/wesleyreisz/todolist/App.java
@@ -12,6 +12,6 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Parse.initialize(this, ApplicationID, ClientKey);
+        Parse.initialize(this);
     }
 }
