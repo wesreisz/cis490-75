e983501d30e57cc831b0491d07c660a18291fcff
Wesley Reisz <wes@wesleyreisz.com>
2015-11-08 12:13:20 -0500

    Added Fragments to the project

---
+++ class-10/homework-part-a/TodoList/app/src/main/java/com/example/wesleyreisz/todolist/fragment/TaskListFragment.java
@@ -0,0 +1,20 @@
+package com.example.wesleyreisz.todolist.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wesleyreisz.todolist.R;
+
+/**
+ * Created by wesleyreisz on 11/8/15.
+ */
+public class TaskListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_task_list, container, false);
+    }
+}
