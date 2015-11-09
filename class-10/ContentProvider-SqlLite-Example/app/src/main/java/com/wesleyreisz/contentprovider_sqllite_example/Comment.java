99e6151b2fd88e428df113e3c27a0527e565b924
Wesley Reisz <wes@wesleyreisz.com>
2015-11-08 21:30:25 -0500

    SQLite Database Example

---
+++ class-10/ContentProvider-SqlLite-Example/app/src/main/java/com/wesleyreisz/contentprovider_sqllite_example/Comment.java
@@ -0,0 +1,30 @@
+package com.wesleyreisz.contentprovider_sqllite_example;
+
+/**
+ * Created by wesleyreisz on 11/8/15.
+ */
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
