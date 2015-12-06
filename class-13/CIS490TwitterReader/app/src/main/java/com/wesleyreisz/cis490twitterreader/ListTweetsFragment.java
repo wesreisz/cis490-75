d7b147cd41cc8a64c801f13fb7d61c0821be6469
Wesley Reisz <wes@wesleyreisz.com>
2015-12-06 16:42:44 -0500

    Added simple listview
    This listview has hardcoded data but demonstrations how we want to populate
    the listview

--- class-13/CIS490TwitterReader/app/src/main/java/com/wesleyreisz/cis490twitterreader/ListTweetsFragment.java
+++ class-13/CIS490TwitterReader/app/src/main/java/com/wesleyreisz/cis490twitterreader/ListTweetsFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,15 @@ public class ListTweetsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list_tweets, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_tweets, container, false);
+
+        ListView listView = (ListView)view.findViewById(R.id.listViewTweets);
+        List<String> tweets = new ArrayList<String>();
+        tweets.add("Hello, I'm a tweet");
+        tweets.add("#l1c4 we are bowling");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,tweets);
+        listView.setAdapter(adapter);
+
+        return view;
     }
 }
