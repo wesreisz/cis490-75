3d75a1764bfe6e09b9d45cebd1dcd19ebf24c603
Wesley Reisz <wes@wesleyreisz.com>
2015-12-06 16:19:42 -0500

    Added some changes from previous class work

--- class-10/homework-part-a/TodoList/app/src/main/java/com/example/wesleyreisz/todolist/MainActivityFragment.java
+++ class-10/homework-part-a/TodoList/app/src/main/java/com/example/wesleyreisz/todolist/MainActivityFragment.java
@@ -1,8 +1,8 @@
 package com.example.wesleyreisz.todolist;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +21,13 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, new CreateFragment());
+        fragmentTransaction.commit();
+
+        return view;
     }
 }
